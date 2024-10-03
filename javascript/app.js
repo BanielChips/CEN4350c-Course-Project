faf21f2bee12b066f9ce9d5c79187e98be814def
Daniel R <danielreecy@gmail.com>
2024-10-02 22:17:14 -0400

    Front-End report commit
    Last update before the front-end report video.

--- javascript/app.js
+++ javascript/app.js
@@ -1,17 +1,17 @@
 
 // methods for login form popup
 function openForm() {
-  document.getElementById("loginForm").style.display = "flex";
+  document.getElementById("login-form").style.display = "flex";
   document.getElementById("overlay").style.display = "block";
 }
 
 function closeForm() {
-  document.getElementById("loginForm").style.display = "none";
+  document.getElementById("login-form").style.display = "none";
   document.getElementById("overlay").style.display = "none";
 }
 
 window.onclick = function(event) {
-  var popup = document.getElementById("loginForm");
+  var popup = document.getElementById("login-form");
   var overlay = document.getElementById("overlay");
   var isPopupVisible = window.getComputedStyle(popup).display !== 'none';
 
@@ -23,11 +23,9 @@ window.onclick = function(event) {
   }
 }
 
+// Shrink text and stick header to top of page on scroll. Revert when at top.
 window.onscroll = function() { stickyHeader() };
-
-
 function stickyHeader() {
-
   if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
     document.getElementById("navBar").style.transition = "0.4s";
     document.getElementById("navBar").style.fontSize = "22px";
@@ -38,3 +36,23 @@ function stickyHeader() {
     document.getElementById("navBar").style.transition = "0.2s";
   }
 }
+
+// Product constructor
+// Creating this here to make product references scalable in the future---
+// When using product data dynamically, the constructor allows
+// database queries to then be mapped to an object and not hard coded
+function Product(pid, productName, productDescription){
+  this.pid = pid;
+  this.productName = productName;
+  this.productDescription = productDescription;
+}
+
+// Until the database is integrated,
+// product objects will be used to display and test page functionality
+const product_hair_wax =
+  new Product("0001", "hair wax", "High hold");
+const product_hair_pomade =
+  new Product("0002", "hair pomade", "Moderate hold. Great texture.");
+const product_hair_grease =
+  new Product("0003", "hair grease", "Moderate hold. Glossy finish.");
+
