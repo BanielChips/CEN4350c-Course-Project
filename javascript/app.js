aa26f23e633cdf574be635686367a9d2129ec60f
Daniel R <danielreecy@gmail.com>
2024-11-06 23:55:06 -0500

    Back-end report update

--- javascript/app.js
+++ javascript/app.js
@@ -1,19 +1,44 @@
-
-// methods for login form popup
 function openForm() {
   document.getElementById("login-form").style.display = "flex";
+  document.getElementById("login-form").style.flexDirection = "column";
+  document.getElementById("login-form").style.alignItems = "center";
+
+  setTimeout(() => {
+    document.getElementById("login-form").style.top = "60px";
+  }, 10);
+
   document.getElementById("overlay").style.display = "block";
 }
 
 function closeForm() {
-  document.getElementById("login-form").style.display = "none";
+  document.getElementById("login-form").style.transition = "top 0.8s ease";
+  document.getElementById("login-form").style.top = "-300px";
+
+  setTimeout(() => {
+    document.getElementById("login-form").style.display = "none";
+  }, 600);
+
+  document.getElementById("overlay").style.display = "none";
+}
+
+function closeRegistration() {
+  document.getElementById("registration-form").style.top = "-1000px";
+
+  setTimeout(() => {
+    document.getElementById("registration-form").style.display = "none";
+  }, 600);
+
   document.getElementById("overlay").style.display = "none";
 }
 
 window.onclick = function(event) {
   var popup = document.getElementById("login-form");
   var overlay = document.getElementById("overlay");
-  var isPopupVisible = window.getComputedStyle(popup).display !== 'none';
+  var registrationOverlay = document.getElementById("registration-form");
+
+  var isPopupVisible = window.getComputedStyle(popup).display !== "none";
+  var isRegistrationVisible = window.getComputedStyle(registrationOverlay) !== "none";
+
 
   // If user clicks off of login popup, close the popup
   if (isPopupVisible) {
@@ -21,10 +46,27 @@ window.onclick = function(event) {
       closeForm();
     }
   }
+
+  if (isRegistrationVisible) {
+    if (overlay.contains(event.target)) {
+      closeRegistration();
+    }
+  }
+
+
 }
 
-// Shrink text and stick header to top of page on scroll. Revert when at top.
-window.onscroll = function() { stickyHeader() };
+document.getElementById("registration-popup").addEventListener("click", function() {
+  document.getElementById("registration-form").style.display = "block";
+  document.getElementById("login-form").style.display = "none";
+
+  setTimeout(() => {
+    document.getElementById("registration-form").style.top = "60px";
+  }, 10);
+});
+
+// Removed nav bar change on scroll to just be default display
+// window.onscroll = function() { stickyHeader() };
 function stickyHeader() {
   if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
     document.getElementById("navBar").style.transition = "0.4s";
@@ -37,24 +79,42 @@ function stickyHeader() {
   }
 }
 
-// Product constructor
-// Creating this here to make product references scalable in the future---
-// When using product data dynamically, the constructor allows
-// database queries to then be mapped to an object and not hard coded
+// TODO:
+//  After - User Authentication:
+//  let cartCount = query(SELECT count(*) FROM cart_items)
+//  (Also consider quantity of each item in users' carts on top of addition to count(*))
+let cartCount = 0;
+function addToCart() {
+  console.log("adding item to cart");
+  cartCount++;
+  const cartQuantity = document.getElementById("cart-count");
+
+  if (cartCount > 10) {
+    document.getElementById("cart-count").style.fontSize = "10px";
+    cartQuantity.textContent = 10 + "+";
+  } else  {
+    cartQuantity.textContent = cartCount.toString();
+  }
+
+  if (cartCount > 0) {
+    cartQuantity.classList.add("active");
+  } else {
+    cartQuantity.classList.remove("active");
+  }
+}
+
+document.querySelector('.add-to-cart').addEventListener("click", addToCart);
+
 function Product(pid, productName, productDescription){
   this.pid = pid;
   this.productName = productName;
   this.productDescription = productDescription;
 }
 
-// Starting to work with data from the SQL server
-document.addEventListener('DOMContentLoaded', (event) => {
-    getProducts();
-});
+let productInformation = [];
 
-
-function getProducts() {
-    fetch('/products')
+function fetchProductData() {
+    return fetch('/get-data')
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response failed');
@@ -62,11 +122,116 @@ function getProducts() {
             return response.json();
         })
         .then(data => {
-            console.log(data);
+            productInformation = data;
         })
         .catch(error => {
             console.error('There was a problem with the fetching table data:', error);
         });
 }
 
+function fetchProductPrice() {
+  fetch('/get-data')
+    .then(response => response.json())
+    .then(data => {
+      document.getElementById('price').textContent = data.price;
+    })
+    .catch(error => {
+      console.error('Error fetching product price:', error);
+    });
+}
 
+function loadProductData(productInformation) {
+  if (productInformation.length > 0) {
+    productInformation.forEach((product, index) => {
+      const productDetails = document.getElementById(`product-${index + 1}`);
+
+
+      if (productDetails) {
+        const productName = productDetails.querySelector('.product-name');
+        const productDescription = productDetails.querySelector('.product-description');
+        const productPrice = productDetails.querySelector('.product-price');
+
+        productName.textContent = product.name;
+        productDescription.textContent = product.description;
+        productPrice.textContent = product.price;
+      }
+    });
+  } else {
+      console.log('No product information found.');
+  }
+}
+
+function loadNewsletter() {
+  const form = document.getElementById('newsletter-signup-form');
+
+  if (form) {
+    form.addEventListener('submit', function(event) {
+      event.preventDefault();
+
+      console.log("Newsletter button pressed");
+
+      const email = document.getElementById('email-input').value;
+      console.log("Submitting email:", email);
+      fetch('/subscribe', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email: email }),
+      })
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          return response.json();
+        })
+        .then(data => {
+          alert('Thank you for subscribing to our newsletter! You will now begin to receive emails with product updates and events.');
+
+          document.getElementById('email-input').value = '';
+        })
+        .catch((error) => {
+          console.error('Newsletter error:', error);
+        });
+    });
+  } else {
+    console.error("Form not found!");
+  }
+}
+
+document.getElementById("registration-form").addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  const firstName = document.getElementById("first-name").value;
+  const lastName = document.getElementById("last-name").value;
+  const email = document.getElementById("registration-email").value;
+  const password = document.getElementById("registration-psw").value;
+
+  fetch("/register", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      firstName,
+      lastName,
+      email,
+      password
+    })
+  })
+    .then(response => response.json())
+    .then(data => {
+      if (data.message) {
+        alert(data.message);
+      } else {
+        alert(data.error || "Registration error");
+      }
+    })
+    .catch(error => console.error("WOAH:", error))
+});
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  fetchProductData().then( () => {
+    loadProductData(productInformation)
+  });
+  loadNewsletter();
+});
--- javascript/server.js
+++ javascript/server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const mysql = require('mysql');
+const bcrypt = require('bcrypt');
 
 const app = express();
 const port = 3000;
 
 app.use(express.static(__dirname));
+app.use(express.json());
 
-// Including credential from a js file ignored by git
+// Including credentials from a js file ignored by git
 const { username, password } = require('./credentials');
 
 const connection = mysql.createConnection({
@@ -39,10 +41,94 @@ app.get('/get-data', (req, res) => {
     });
 });
 
+app.post('/subscribe', (req, res) => {
+  const email = req.body.email;
 
+  if (!email) {
+    return res.status(400).json({ error: 'Email is required' });
+  }
 
+  const query = 'INSERT INTO newsletter (email) VALUES (?)';
+  connection.query(query, [email], (err, result) => {
+    if (err) {
+      return res.status(500).json({ error: 'Database error' });
+    }
+    res.status(200).json({ message: 'Email succesfully subscribed!' });
+  });
+});
 
-// Start the server
+app.post('/register', (req, res) => {
+  const { firstName, lastName, email, password } = req.body;
+
+  if (!firstName || !lastName || !email || !password) {
+    return  res.status(400).json({ error: '(first, last, email, password) are required' });
+  }
+
+  connection.query("SELECT * FROM users WHERE email = ?", [email], (err, results) => {
+    if (err) {
+      console.error('Error finding email', err);
+      return res.status(500).send('Database error');
+    }
+
+    if (results.length > 0) {
+      return res.status(400).json({ error: 'Email already exists' });
+    }
+
+    bcrypt.hash(password, 10, (err, hashedPass) => {
+      if(err) {
+        consle.error('Error jumbling password');
+        return res.status(500).send('Potato error');
+      }
+
+      const query = 'INSERT INTO users (firstName, lastName, email, password) VALUES (?, ?, ?)';
+      connection.query(query, [name, email, hashedPass], (err, results) => {
+        if(err) {
+          console.error('Error submitting to database');
+          return res.status(500).send('Database error');
+        }
+
+        res.status(200).json({ message: 'User Registered'});
+      });
+    });
+  });
+});
+
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return req.status(400).json({error: 'Username and password required'});
+  }
+
+  connection.query('SELECT * FROM users WHERE email = ?', [email], (err, results) => {
+    if (err) {
+      console.error('A user with the given email has not been found');
+      return res.status(500).send('Verification error');
+    }
+
+    if (results.length === 0) {
+      return  res.status(400).json({error: 'User email not found'});
+    }
+
+    const user = results[0];
+
+    bcrypt.compare(password, user.password, (err, passMatch) => {
+      if (err) {
+        console.error('Error with encryption compare', err);
+        return res.status(500).send('Server error');
+      }
+
+      if (!passMatch) {
+        return res.status(400).send('Password not valid');
+      }
+
+      res.status(200).json({message: 'Login successful'});
+    })
+
+  });
+
+})
+
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
